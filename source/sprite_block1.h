febcd0dab6387a77712c94eac9bed7d3d9524bc3
kyun34@gmail.com <kyun34@gmail.com>
2019-05-05 12:44:51 +0900

    グラフィック修正

--- source/sprite_block1.h
+++ source/sprite_block1.h
@@ -3,11 +3,11 @@
 
 //======================================================================
 //
-//	sprite_block1, 40x40@8, 
-//	+ 25 tiles not compressed
-//	Total size: 1600 = 1600
+//	sprite_block1, 64x64@8, 
+//	+ 64 tiles not compressed
+//	Total size: 4096 = 4096
 //
-//	Time-stamp: 2019-05-05, 12:39:20
+//	Time-stamp: 2019-05-05, 12:43:59
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -16,8 +16,8 @@
 #ifndef GRIT_SPRITE_BLOCK1_H
 #define GRIT_SPRITE_BLOCK1_H
 
-#define sprite_block1TilesLen 1600
-extern const unsigned short sprite_block1Tiles[800];
+#define sprite_block1TilesLen 4096
+extern const unsigned short sprite_block1Tiles[2048];
 
 #endif // GRIT_SPRITE_BLOCK1_H
 
